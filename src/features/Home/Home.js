7770a16f780993aeaa2123fae71ff5c4af6cf527
Daniel Espinosa <105755582+dmousy@users.noreply.github.com>
2022-07-19 08:58:15 -0500

    skeleton and moment install

---
+++ src/features/Home/Home.js
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AnimatedList } from 'react-animated-list';
+import Post from '../Post/Post';
+import PostLoading from '../Post/PostLoading';
+import getRandomNumber from '../../utils/getRandomNumber';
+import { fetchPosts, selectFilteredPost, setSearchTerm, fetchComments } from '../../store/redditSlice';
+import './Home.css';
+
+const Home = () => {
+    const reddit = useSelector((state) => state.reddit);
+    const { isLoading, error, searchTerm, selectedSubreddit } = reddit;
+    const post = useSelector(selectFilteredPost);
+    const dispatch = useDispatch();
+
+    useEffect (() => {
+        dispatch(fetchPosts(selectedSubreddit));
+    }, [selectedSubreddit]);
+
+    const onToggleComments = (index) => {
+        const getComments = (permalink) => {
+            dispatch(fetchComments(index, permalink));
+        };
+
+        return getComments;
+    };
+
+    if (isLoading) {
+        return (
+            <AnimatedList animation="zoom">
+                {Array(getRandomNumber(3, 10)).fill(<PostLoading />)}
+            </AnimatedList>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className="error">
+                <h2>Failed to load post.</h2>
+                <button
+                    type="button"
+                    onClick={() => dispatch(fetchPosts(selectedSubreddit))} >
+                        Try again
+                    </button>
+            </div>
+        );
+    }
+
+    if (post.length === 0) {
+        return (
+            <div className="error">
+                <h2>No post matching "{searchTerm}"</h2>
+                <button type="button" onClick={() => dispatch(setSearchTerm(''))} >
+                    Go home
+                </button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            {post.map((post, index) => (
+                <Post
+                    key={post.id}
+                    post={post}
+                    onToggleComments={onToggleComments(index)} />
+            ))}
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/store/redditSlice.js
@@ -0,0 +1,112 @@
+import { createSlice, createSelector, bindActionCreators } from '@reduxjs/toolkit';
+import { getSubredditPost, getPostComments, getSubredditPosts } from '../api/reddit';
+
+const initialState = {
+    post: [],
+    error: false,
+    isLoading: false,
+    searchTerm: '',
+    selectedSubreddit: '/r/pixelsart',
+};
+
+const redditSlice = createSlice ({
+    name: 'redditPosts',
+    initialState,
+    reducers: {
+        setPost(state, action) {
+            state.posts = action.payload;
+        },
+        startGetPost(state) {
+            state.isLoading = true;
+            state.error = false;
+        },
+        getPostSuccess(state, action) {
+            state.isLoading = false;
+            state.post = action.payload;
+        },
+        getPostFailed(state) {
+            state.isLoading = false;
+            state.error = true;
+        },
+        setSearchTerm(state, action) {
+            state.searchTerm = action.payload;
+        },
+        setSelectedSubreddit(state, action) {
+            state.selectedSubreddit = action.payload;
+            state.searchTerm = '';
+        },
+        toggleShowingComments(state, action) {
+            state.post[action.payload].showingComments = !state.post[action.payload].showingComments;
+        },
+        stateGetComments(state, action) {
+            //don't fetch comments if we're hidding them
+            state.post[action.payload].showingComments = !state.post[action.payload].showingComments;
+            if (!state.post[action.payload].showingComments) {
+                return;
+            }
+            state.posts[action.payload].loadingComments = false;
+            state.posts[action.payload].error = true;
+        },
+    },
+});
+
+export const {
+    setPost,
+    getPostsFailed,
+    getPostsSuccess,
+    startGetPosts,
+    setSearchTerm,
+    setSelectedSubreddit,
+    toggleShowingComments,
+    getCommentsFailed,
+    getCommentsSuccess,
+    startGetComments
+} = redditSlice.actions;
+
+export default redditSlice.reducer;
+
+//redux thunk for comments
+
+export const fetchPosts = (subreddit) => async (dispatch) => {
+    try {
+        dispatch(startGetPosts());
+        const post = await getSubredditPosts(subreddit);
+
+        //adding ShowingComments and comments as additional fields when user wants to showing them. Needs to call to another API endpoint for each post's comments
+        const postsWithMetadata = posts.map((post) => ({
+            ...post,
+            showingComments: false,
+            comments: [],
+            loadingComments: false,
+            errorComments: false,
+        }));
+        dispatch(getPostsSuccess(postsWithMetadata));
+    } catch (error) {
+        dispatch(getPostsFailed());
+    }
+};
+
+export const fetchComments = (index, permalink) => async (dispatch) => {
+    try {
+        dispatch(startGetComments(index));
+        const comments = await getPostComments(permalink);
+        dispatch(getCommentsSuccess({index, comments}));
+    } catch (error) {
+        dispatch(getCommentsFailed(index));
+    }
+};
+
+const selectPosts = (state) => state.reddit.posts;
+const selectSearchTerm = (state) => state.redit.searchTerm;
+export const selectSelectedSubreddit = (state) => state.reddit.selectedSubreddit;
+
+export const selectFilteredPost = createSelector (
+    [selectPosts, selectSearchTerm], (posts, searchTerm) => {
+        if (searchTerm !== '') {
+            return posts.filter((post) => 
+                post.title.toLoweCase().includes(searchTerm.toLoweCase())
+            )
+        }
+        return posts;
+    }
+)
